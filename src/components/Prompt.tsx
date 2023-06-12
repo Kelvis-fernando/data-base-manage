62473e0debdf1bcf7e5a655171ead1ce45aa5d4a
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-06-12 16:01:26 -0300

    feat: created the prompt comp and styled

---
+++ src/components/Prompt.tsx
@@ -0,0 +1,11 @@
+import { Box, Textarea } from "@chakra-ui/react";
+
+const Prompt = () => {
+  return (
+    <Box border="1px solid black" p="1" w="70%" h="90vh">
+      <Textarea h="100%"></Textarea>
+    </Box>
+  );
+};
+
+export default Prompt;
