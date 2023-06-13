b3009a804e7483b12b3e5c1737742205ff0d6fe3
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-06-13 09:15:43 -0300

    feat: added react router dom to app, for control the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Flex } from "@chakra-ui/react";
 import Sidebar from "./components/Sidebar";
+import ConnectForm from "./pages/ConnectForm";
 import Console from "./pages/Console";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Flex justifyContent="center" alignItems="center" h="100vh">
       <Sidebar />
-      <Console />
+      <Router>
+        <Routes>
+          <Route path="/" element={<ConnectForm />} />
+          <Route path="/console" element={<Console />} />
+        </Routes>
+      </Router>
     </Flex>
   );
 };
