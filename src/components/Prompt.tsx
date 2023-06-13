05c4e3599925418a7f92cfe59f47c681a3ae08a7
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-06-13 15:27:17 -0300

    chore: add the button to send a query in console, and styled the list in tabs

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,11 +1,28 @@
-import { Box, Heading, Textarea } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Textarea } from "@chakra-ui/react";
 import ResultQuery from "./ResultQuery";
+import { useState } from "react";
 
 const Prompt = () => {
+  const [query, setQuery] = useState<string>();
+
   return (
-    <Box border="1px solid black" p="1" w="70%" h="90vh">
-      <Heading>PROMPT</Heading>
-      <Textarea h="55%"></Textarea>
+    <Box border="1px solid black" p="1" w="70%" h="90vh" position="relative">
+      <Flex justifyContent="space-between">
+        <Heading>PROMPT</Heading>
+        <Button
+          bg="orange"
+          _hover={{ bg: "orange.300" }}
+          fontWeight="bold"
+          type="submit"
+        >
+          SEND QUERY
+        </Button>
+      </Flex>
+      <Textarea
+        name="query"
+        h="55%"
+        onChange={(event) => setQuery(event.target.value)}
+      ></Textarea>
       <ResultQuery />
     </Box>
   );
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -5,45 +5,44 @@ const Tabs = () => {
   return (
     <Box border="1px solid black" p="1" w="20%" h="90vh" borderRadius="3">
       <Heading>TABS</Heading>
-      <Flex flexDirection="column" p="1">
-        <Flex
-          fontSize="xl"
-          borderBottom="1px solid gray"
-          cursor="pointer"
-          mb="1"
-          alignItems="center"
-          justifyContent="center"
-          bg="gray.200"
-        >
-          <Text fontWeight="bold">Query to select all my users</Text>
-          <X size={20} color="red" fontWeight="bold" />
-        </Flex>
+      <Flex
+        fontSize="sm"
+        p="2"
+        borderBottom="1px solid gray"
+        cursor="pointer"
+        mt="5"
+        mb="1"
+        alignItems="center"
+        justifyContent="center"
+        bg="gray.200"
+      >
+        <Text fontWeight="bold">Query to select all my users</Text>
+        <X size={20} color="red" fontWeight="bold" />
       </Flex>
-      <Flex flexDirection="column" borderRadius="3" p="1">
-        <Flex
-          fontSize="xl"
-          borderBottom="1px solid gray"
-          cursor="pointer"
-          mb="1"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Text fontWeight="bold">Query to my postgreSQL DB</Text>
-          <X size={20} color="red" fontWeight="bold" />
-        </Flex>
+      <Flex
+        fontSize="sm"
+        p="2"
+        borderBottom="1px solid gray"
+        cursor="pointer"
+        mb="1"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text fontWeight="bold">Query to my postgreSQL DB</Text>
+        <X size={20} color="red" fontWeight="bold" />
       </Flex>
-      <Flex flexDirection="column" borderRadius="3" p="1">
-        <Flex
-          fontSize="xl"
-          borderBottom="1px solid gray"
-          cursor="pointer"
-          mb="1"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Text fontWeight="bold">Join into dashboard table</Text>
-          <X size={20} color="red" fontWeight="bold" />
-        </Flex>
+
+      <Flex
+        fontSize="sm"
+        p="2"
+        borderBottom="1px solid gray"
+        cursor="pointer"
+        mb="1"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text fontWeight="bold">Join into dashboard table</Text>
+        <X size={20} color="red" fontWeight="bold" />
       </Flex>
     </Box>
   );
